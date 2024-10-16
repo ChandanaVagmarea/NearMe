11f2e006a3fddfd91a920a0b7d44f559361a19ef
ChandanaVagmarea <chandanavagmare@gmail.com>
2024-10-16 21:13:37 +0530

    Add files via upload

---
+++ contactus.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NearMe-Contact</title>
+    <?php require('inc/links.php');?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
+    
+</head>
+<body class="bg-light">
+<!--header-->    
+    <?php require('inc/header.php');?>
+    <div class="my-5 px-4">
+        <h2 class="fw-bold h-font text-center">CONTACT US</h2>
+        <div class="h-line bg-dark"></div>
+        <p class="text-center mt-3 ">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam,
+              officia expedita fugit!
+        </p>
+    </div>  
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 mb-5 px-4">
+                <div class=" bg-white rounded shadow p-4">
+                    <iframe class="w-100 rounded"height="320px" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3803.8546933933644!2d78.45266927463076!3d17.562103497749412!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb8f9ee1fbbf3d%3A0xd8188b5c2a15ca92!2sMRCET%20CANTEEN%2C%20MALLA%20REDDY%20COLLEGE%2C%20Maisammaguda%2C%20Dulapally%2C%20Hyderabad%2C%20Telangana%20500043!5e0!3m2!1sen!2sin!4v1728842438988!5m2!1sen!2sin"  
+                        loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 mb-5 px-4">
+                <div class=" pop bg-white rounded shadow p-4 border-top border-4 border-dark">
+                    <div class="d-flex align-items-center mb-2">
+                        <img src="images/facilities/1.png" width="40px">
+                        <h5 class="m-0 ms-3 ">Wifi</h5>
+                    </div>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, excepturi. 
+                        Perspiciatis fuga laboriosam perferendis aperiam dolore!</p>
+                </div>
+            </div>
+
+            
+        </div>
+    </div>
+<!--footer-->
+    <?php require('inc/footer.php');?> 
+</body>
+</html>
